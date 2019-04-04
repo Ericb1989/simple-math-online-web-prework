58a8a3a99538496062879ff66441fe1c7806f44b
Eric Bodden <ericbodden30@gmail.com>
2019-04-04 21:02:24 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -16,6 +16,6 @@ end
 def square_root(81)). to eq(9)
 end
 
-def order_of_operation(num1, num2, num3, num4)
+def order_of_operation((7, 43, 23, 83)).to eq(18))
   #Hint:  __ + (( __ * __ ) / __ )
 end
